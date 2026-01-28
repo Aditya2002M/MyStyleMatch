5ec8d4a216318a29d63b758599bff1a62e3e22a1
AdityaModak22 <amodak@samprino.com>
2026-01-28 17:49:34 +0530

    add files

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import WaitlistPage from "@/components/mvpblocks/waitlist";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <WaitlistPage></WaitlistPage>
+  );
+}
